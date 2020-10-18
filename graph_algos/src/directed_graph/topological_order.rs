7e50c7df4b70a4dac3258e103c6f26959672754d
tolumide-ng <tolumideshopein@gmail.com>
2020-10-18 01:38:18 +0100

    graph: add test to topological sort

--- graph_algos/src/directed_graph/topological_order.rs
+++ graph_algos/src/directed_graph/topological_order.rs
@@ -2,7 +2,10 @@ pub fn execute() {
     use Graphs;
 
     // Graphs::DAG::new(vec![(4, 3), (1, 2), (4, 1), (3, 1)]);
-    Graphs::DAG::new(vec![(1, 2), (1, 4), (2, 3), (2, 4), (4, 3), (4, 5)]);
+    // Graphs::DAG::new(vec![(1, 2), (1, 4), (2, 3), (2, 4), (4, 3), (4, 5)]);
+    let result = Graphs::DAG::new(vec![(0, 2), (1, 2), (2, 3), (3, 4)]);
+
+    println!("THE RESULT >>>  {:?}", result);
 }
 
 mod Graphs {
@@ -13,7 +16,7 @@ mod Graphs {
     }
 
     impl DAG {
-        pub fn new(graph_info: Vec<(u8, u8)>) {
+        pub fn new(graph_info: Vec<(u8, u8)>) -> Vec<u8> {
             // DirectedGraph { graph: None }
             let mut adjacency_list: HashMap<u8, Vec<u8>> = HashMap::new();
             let graph = graph_info.get(0..);
@@ -24,35 +27,49 @@ mod Graphs {
             let the_graph = DAG {
                 graph: Some(adjacency_list),
             };
-            the_graph.get_topological_order();
+            return the_graph.get_topological_order();
         }
 
-        pub fn get_topological_order(&self) {
+        pub fn get_topological_order(&self) -> Vec<u8> {
             let source_nodes = self.graph.as_ref().unwrap().keys();
             let mut stack: Vec<u8> = vec![];
-            let mut visited: HashMap<u8, bool> = HashMap::new();
+            // let mut visited: HashMap<u8, bool> = HashMap::new();
 
             for node in source_nodes {
-                if visited.get(node) == None {
-                    self.get_order(node, &mut stack, &mut visited);
-                }
+                // if visited.get(node) == None {
+                self.get_order(node, &mut stack);
+                // }
             }
             stack.reverse();
             println!("THE STACK!! {:?}", stack);
+            return stack;
         }
 
-        pub fn get_order(&self, node: &u8, stack: &mut Vec<u8>, visited: &mut HashMap<u8, bool>) {
+        pub fn get_order(&self, node: &u8, stack: &mut Vec<u8>) {
             let receiving_nodes = self.graph.as_ref().unwrap().get(node);
-            if visited.get(node) == None {
-                if receiving_nodes != None {
-                    for value in receiving_nodes.unwrap() {
-                        self.get_order(value, stack, visited);
-                    }
-                }
-                if !stack.contains(node) {
-                    stack.push(*node);
+            // if visited.get(node) == None {
+            if receiving_nodes != None {
+                for value in receiving_nodes.unwrap() {
+                    self.get_order(value, stack);
                 }
             }
+            if !stack.contains(node) {
+                stack.push(*node);
+            }
+            // }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Graphs;
+
+    #[test]
+    fn test_topological_order() {
+        let mut result = Graphs::DAG::new(vec![(0, 2), (1, 2), (2, 3), (3, 4)]);
+        assert_eq!(result.pop(), Some(4));
+        assert_eq!(result.pop(), Some(3));
+        assert_eq!(result.pop(), Some(2));
     }
 }
--- graph_algos/src/main.rs
+++ graph_algos/src/main.rs
@@ -1,14 +1,14 @@
-// mod directed_graph;
+mod directed_graph;
 // mod undirected_graph;
-mod bfs;
+// mod bfs;
 
 // use undirected_graph::exit_maze::execute;
 // use undirected_graph::add_exits::execute;
 // use directed_graph::detect_cycle::execute;
-// use directed_graph::topological_order::execute;
+use directed_graph::topological_order::execute;
 // use directed_graph::strongly_connected_comp::execute;
 // use bfs::min_flight_segments::execute;
-use bfs::bipartite::execute;
+// use bfs::bipartite::execute;
 
 fn main() {
     execute();
