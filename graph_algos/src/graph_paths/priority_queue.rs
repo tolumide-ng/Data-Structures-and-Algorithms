cd7fff15066ba4764ad6fdd4fa88d5fb237947c9
tolumide-ng <tolumideshopein@gmail.com>
2020-10-30 23:31:47 +0100

    graph: fix bugs on priority queue

--- graph_algos/src/graph_paths/priority_queue.rs
+++ graph_algos/src/graph_paths/priority_queue.rs
@@ -1,13 +1,27 @@
+pub fn execute() {
+    let mut bin_heap = BinaryHeap::new(20);
+    bin_heap.insert((2, "f"));
+    bin_heap.insert((10, "f"));
+    bin_heap.insert((5, "f"));
+    bin_heap.insert((1, "f"));
+    bin_heap.insert((12, "f"));
+    bin_heap.insert((2, "f"));
+    println!("EXTRACT MIN {:?}", bin_heap.extract_min());
+    bin_heap.remove(1);
+}
+
 #[derive(Debug, Clone)]
 pub struct BinaryHeap<T> {
-    heap: Vec<(u8, T)>,
+    heap: Vec<(i8, T)>,
     max_size: usize,
     size: usize,
 }
 
+use std::fmt::{Debug, Display};
+
 impl<T> BinaryHeap<T>
 where
-    T: PartialEq + Copy + Clone,
+    T: PartialEq + Copy + Clone + Display + Debug,
 {
     pub fn new(max_size: usize) -> Self {
         BinaryHeap {
@@ -29,6 +43,9 @@ where
     }
 
     pub fn get_left_child(&self, index: usize) -> Option<usize> {
+        if (index == 0 && self.size > 0) {
+            return Some(1);
+        }
         if index * 2 < self.size {
             return Some(index * 2);
         } else {
@@ -37,8 +54,11 @@ where
     }
 
     pub fn get_right_child(&self, index: usize) -> Option<usize> {
+        if (index == 0 && self.size > 0) {
+            return Some(index + 2);
+        }
         if (index * 2) + 1 < self.size {
-            return Some(index * 2);
+            return Some(index * 2 + 1);
         }
         None
     }
@@ -63,16 +83,30 @@ where
     pub fn sift_down(&mut self, index: usize) {
         let mut new_index = index;
 
-        // if self.get_left_child(index).unwrap() < self. {}
-        let left_child_index = self.get_left_child(index).unwrap();
-        if (left_child_index <= self.size) && (self.heap[index].0 > self.heap[left_child_index].0) {
-            new_index = left_child_index;
+        let left_child_index = self.get_left_child(index);
+
+        match left_child_index {
+            Some(left_child_index) => {
+                if (left_child_index <= self.size)
+                    && (self.heap[new_index].0 > self.heap[left_child_index].0)
+                {
+                    new_index = left_child_index;
+                }
+            }
+            None => {}
         }
 
-        let right_child_index = self.get_right_child(index).unwrap();
-        if (right_child_index <= self.size) && (self.heap[index].0 > self.heap[right_child_index].0)
-        {
-            new_index = right_child_index;
+        let right_child_index = self.get_right_child(index);
+
+        match right_child_index {
+            Some(right_child_index) => {
+                if (right_child_index <= self.size)
+                    && (self.heap[new_index].0 > self.heap[right_child_index].0)
+                {
+                    new_index = right_child_index;
+                }
+            }
+            None => {}
         }
 
         if new_index != index {
@@ -81,11 +115,12 @@ where
 
             std::mem::replace(&mut self.heap[new_index], curr_val);
             std::mem::replace(&mut self.heap[index], child);
+
             self.sift_down(new_index);
         }
     }
 
-    pub fn insert(&mut self, value: (u8, T)) {
+    pub fn insert(&mut self, value: (i8, T)) {
         if self.size != self.max_size {
             self.size += 1;
             self.heap.push(value);
@@ -93,7 +128,7 @@ where
         }
     }
 
-    pub fn extract_min(&mut self) -> Option<(u8, T)> {
+    pub fn extract_min(&mut self) -> Option<(i8, T)> {
         if self.heap.len() > 1 {
             let result = self.heap[0];
             let last_value = self.heap.pop().unwrap();
@@ -105,17 +140,17 @@ where
         return None;
     }
 
-    pub fn remove(&mut self, index: usize) -> Option<(u8, T)> {
+    pub fn remove(&mut self, index: usize) -> Option<(i8, T)> {
         if index < self.size {
             let result = self.heap[index];
-            std::mem::replace(&mut self.heap[index], (f64::NEG_INFINITY as u8, result.1));
+            std::mem::replace(&mut self.heap[index], (f64::NEG_INFINITY as i8, result.1));
             self.sift_up(index);
             return self.extract_min();
         }
         return None;
     }
 
-    pub fn change_priority(&mut self, index: usize, new_value: (u8, T)) {
+    pub fn change_priority(&mut self, index: usize, new_value: (i8, T)) {
         let old_value = self.heap[index];
         std::mem::replace(&mut self.heap[index], new_value);
         if new_value.0 > old_value.0 {
