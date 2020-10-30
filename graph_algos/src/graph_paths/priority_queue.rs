2e9130690a22ce75202db405607a64350703af65
tolumide-ng <tolumideshopein@gmail.com>
2020-10-30 11:25:00 +0100

    graph algo: complete priority queue for prims algorithm

--- graph_algos/src/graph_paths/priority_queue.rs
+++ graph_algos/src/graph_paths/priority_queue.rs
@@ -61,26 +61,67 @@ where
     }
 
     pub fn sift_down(&mut self, index: usize) {
-        let mut max_index = index;
+        let mut new_index = index;
 
         // if self.get_left_child(index).unwrap() < self. {}
         let left_child_index = self.get_left_child(index).unwrap();
-        if left_child_index <= self.size && self.heap[index].0 > self.heap[left_child_index].0 {
-            max_index = left_child_index;
+        if (left_child_index <= self.size) && (self.heap[index].0 > self.heap[left_child_index].0) {
+            new_index = left_child_index;
         }
 
         let right_child_index = self.get_right_child(index).unwrap();
-        if right_child_index <= self.size && self.heap[index].0 > self.heap[right_child_index].0 {
-            max_index = right_child_index;
+        if (right_child_index <= self.size) && (self.heap[index].0 > self.heap[right_child_index].0)
+        {
+            new_index = right_child_index;
         }
 
-        if max_index != index {
-            let child = self.heap[max_index];
+        if new_index != index {
+            let child = self.heap[new_index];
             let curr_val = self.heap[index];
 
-            std::mem::replace(&mut self.heap[max_index], curr_val);
-            std::mem::replace(&mut self.heap[index], curr_val);
+            std::mem::replace(&mut self.heap[new_index], curr_val);
+            std::mem::replace(&mut self.heap[index], child);
+            self.sift_down(new_index);
+        }
+    }
+
+    pub fn insert(&mut self, value: (u8, T)) {
+        if self.size != self.max_size {
+            self.size += 1;
+            self.heap.push(value);
+            self.sift_up(self.size - 1);
+        }
+    }
+
+    pub fn extract_min(&mut self) -> Option<(u8, T)> {
+        if self.heap.len() > 1 {
+            let result = self.heap[0];
+            let last_value = self.heap.pop().unwrap();
+            std::mem::replace(&mut self.heap[0], last_value);
+            self.size -= 1;
+            self.sift_down(0);
+            return Some(result);
+        }
+        return None;
+    }
+
+    pub fn remove(&mut self, index: usize) -> Option<(u8, T)> {
+        if index < self.size {
+            let result = self.heap[index];
+            std::mem::replace(&mut self.heap[index], (f64::NEG_INFINITY as u8, result.1));
+            self.sift_up(index);
+            return self.extract_min();
+        }
+        return None;
+    }
+
+    pub fn change_priority(&mut self, index: usize, new_value: (u8, T)) {
+        let old_value = self.heap[index];
+        std::mem::replace(&mut self.heap[index], new_value);
+        if new_value.0 > old_value.0 {
             self.sift_down(index);
+        } else {
+            self.sift_up(index);
         }
     }
 }
